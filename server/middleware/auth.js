bb95539ebf2e828bcd1e26490310f835ea1b7f74
cjvil <christiejvillanueva@gmail.com>
2017-09-20 18:29:10 -0700

    Implement cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,11 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  console.log('inside session parser');
+  models.Session.create()
+  .then(() => {
+    res.end();
+  });
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
 const parseCookies = (req, res, next) => {
+  console.log(req.headers.cookie);
+  var cookie = req.headers.cookie;
+
+  // req.cookies = {cookie: cookie};
+  
+  if (!cookie) {
+    console.log('no cookie found');
+    req.cookies = {};
+    // next();
+  } else {
+    req.cookies = {cookie: cookie};
+  }
 };
 
 module.exports = parseCookies;
