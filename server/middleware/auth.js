8db17643e1b88d69bed51108a2330adc463e70b4
cjvil <christiejvillanueva@gmail.com>
2017-09-21 15:49:58 -0700

    Reject bad cookies and replace with new session/cookie

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,8 @@
 const models = require('../models');
+const crypto = require('../lib/hashUtils.js');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  // console.log('cookies: ', req.cookies);
-
   if (Object.keys(req.cookies).length === 0) {
     console.log('no cookies found');
 
@@ -21,8 +20,7 @@ module.exports.createSession = (req, res, next) => {
         req.session.user.userId = session.userId;
 
         console.log(session);
-        console.log(req.session); // this is fine, why not inside test?
-
+        console.log(req.session);
         return models.Users.get({id: session.userId});      
       })
       .then((user) => {
@@ -40,6 +38,49 @@ module.exports.createSession = (req, res, next) => {
       });
   } else {
     console.log('cookies found');
+
+    // check if cookies on req are in database
+    // if not
+      // clear cookies
+      // create new cookie and reassign to req
+    models.Sessions.get({hash: req.cookies.shortlyid})
+      .then((session) => {
+        if (!session) {
+          models.Sessions.create()
+            .then((result) => {
+              return models.Sessions.get({id: result.insertId});
+            })
+            .then((session) => {
+              res.cookies = {
+                shortlyid: {value: session.hash}
+              };
+
+              req.session = {hash: session.hash};
+              req.session.user = {};
+              req.session.user.userId = session.userId;
+
+              console.log(session);
+              console.log(req.session);
+              return models.Users.get({id: session.userId});      
+            })
+            .then((user) => {
+              console.log('user: ', user);
+              if (user) {
+                req.session.user.username = user.username;
+              } else {
+                req.session.user.username = null;
+              }
+
+              next();
+            })
+            .catch((err) => {
+              console.log('error', err);
+            });
+
+
+        }
+      });
+
     req.session = {
       hash: req.cookies.shortlyid,
       user: {}
@@ -48,11 +89,9 @@ module.exports.createSession = (req, res, next) => {
     // look up session by hash
     models.Sessions.get({hash: req.cookies.shortlyid})
       .then((session) => {
-        console.log('with cookies session', session);
         req.session.userId = session.userId;
         if (session.user) {
           req.session.user.username = session.user.username;
-          console.log('with cookies req.session', req.session);
           return models.Users.get({id: session.userId});
         } else {
           return {
@@ -63,7 +102,6 @@ module.exports.createSession = (req, res, next) => {
       })
       .then((user) => {
         req.session.user.username = user.username;
-        console.log('session' + JSON.stringify(req.session));
         next();
       })
       .catch((err) => {
